d6ca4f729f7aeda7a6c5b87c8b0e928a0de3ffa1
Wendy Breiding <webreidi@microsoft.com>
2023-05-03 16:35:58 -0700

    Initial Push

---
+++ BlazorSample_WebAssembly/ComponentMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorSample;
+
+public class ComponentMetadata
+{
+    public string? Name { get; set; }
+    public Dictionary<string, object> Parameters { get; set; } =
+        new Dictionary<string, object>();
+}
